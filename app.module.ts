2a351c850498de96152cca891465edc13f8dba63
Jaime Leon <jaime.leon.viveros@gmail.com>
2021-08-26 21:35:55 -0500

    Primer Commit

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+import { heroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    heroesModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
